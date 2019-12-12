030849fdfe42e99b05395071e1b6e0697669d538
supfiger <danilzin777@gmail.com>
2019-12-12 22:02:48 +0200

    Updates

---
+++ precache-manifest.607b5578fece54c517fe8713f7262cc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b8de637356136fb75fa609fdd1fdef",
+    "url": "/list-of-products/index.html"
+  },
+  {
+    "revision": "2c99ff5ebeb686ec25af",
+    "url": "/list-of-products/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "cf50a47f13c8be9d8e17",
+    "url": "/list-of-products/static/css/main.b60df32e.chunk.css"
+  },
+  {
+    "revision": "2c99ff5ebeb686ec25af",
+    "url": "/list-of-products/static/js/2.5ab441ca.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/list-of-products/static/js/2.5ab441ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf50a47f13c8be9d8e17",
+    "url": "/list-of-products/static/js/main.bddc75ed.chunk.js"
+  },
+  {
+    "revision": "5ccacb29f1aacad15a58",
+    "url": "/list-of-products/static/js/runtime-main.d935e5d6.js"
+  }
+]);
--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as ProductListPage } from "./ProductListPage/ProductListPage";
-export { default as ProductPage } from "./ProductPage/ProductPage";
-export { default as RegisterPage } from "./RegisterPage/RegisterPage";
-export { default as LoginPage } from "./LoginPage/LoginPage";
-
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as ModalComponent } from "./ModalComponent/ModalComponent";
-export { default as ReviewModal } from "./ReviewModal/ReviewModal";
-export { default as Review } from "./Review/Review";
